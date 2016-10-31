6aba74b6c9057d9ce4965e72cd34fc4741c3671b
JEONHEESOO <heelight@naver.com>
2016-10-31 14:43:21 +0900

    first commit

---
+++ Add.java
@@ -0,0 +1,26 @@
+package Mid;
+
+import java.util.Scanner;
+
+public class Add {
+	public static void main(String[] args) {
+
+		
+		Scanner input = new Scanner(System.in);
+
+		int x;
+		int y;
+		int sum;
+
+		System.out.println("첫 번째 숫자를 입력하시오: ");
+		x = input.nextInt();
+
+		System.out.println("두 번째 숫자를  입력하시오: ");
+		y = input.nextInt();
+
+		sum = x + y;
+
+		System.out.println(sum);
+	}
+
+}
